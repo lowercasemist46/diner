b892113945973e1201973961a69e2ef49f3c7471
Timothy Bartsch <timothybartsch0929@gmail.com>
2023-05-02 11:00:25 -0400

    Added April 25th Class Updated Code

--- index.php
+++ index.php
@@ -19,11 +19,56 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function(){
-    echo  '<h1>Welcome to My Diner!</h1>';
+    //echo  '<h1>Welcome to My Diner!</h1>';
 
     //Display a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Define a breakfast route
+$f3->route('GET /breakfast', function(){
+//    echo  '<h1>Breakfast Menu</h1>';
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/menus/breakfast.html');
+});
+// Define a order route
+$f3->route('GET|POST /order1', function($f3){
+//    echo  '<h1>Breakfast Menu</h1>';
+
+    //If the form has been posted
+    if($_SERVER['REQUEST_METHOD']== "POST") {
+        // Get the data
+
+        // Validate the data
+
+        //Store the data in the session array
+
+        //Redirect to order2 route
+        $f3->reroute('order2');
+    }
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+});
+// Define a order2 route
+$f3->route('GET /order2', function(){
+//    echo  '<h1>Breakfast Menu</h1>';
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/orderForm2.html');
+});
+// Define a summary route
+$f3->route('GET /summary', function(){
+//    echo  '<h1>Breakfast Menu</h1>';
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 // Run Fat-Free
 $f3->run();
